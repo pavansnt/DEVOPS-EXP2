8235e80017a8919b07e57d0e5581342c426790f5
pavansnt <pavansnnt55@gmail.com>
2025-04-22 14:34:27 +0530

    added some docs

---
+++ src/main/resources/script.js
@@ -0,0 +1,13 @@
+function validateLogin(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username === "admin" && password === "1234"){
+        window.location.href = "home.html";
+        return false;
+    }
+    else{
+        alert("Invalid username or password!");
+        return false;
+
+}
+}
